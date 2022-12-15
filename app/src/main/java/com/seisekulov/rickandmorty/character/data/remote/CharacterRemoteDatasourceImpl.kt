7d16c91765539f78d73bab2b33442bfe3c908e5c
Dias Seisekulov <seysekulov@mail.ru>
2022-12-15 16:04:05 +0600

    deleted empty line

--- app/src/main/java/com/seisekulov/rickandmorty/character/data/remote/CharacterRemoteDatasourceImpl.kt
+++ app/src/main/java/com/seisekulov/rickandmorty/character/data/remote/CharacterRemoteDatasourceImpl.kt
@@ -3,7 +3,6 @@ package com.seisekulov.rickandmorty.character.data.remote
 import com.seisekulov.rickandmorty.character.model.Character
 import javax.inject.Inject
 
-
 class CharacterRemoteDatasourceImpl @Inject constructor() : CharacterRemoteDatasource {
     override suspend fun getCharacters(): List<Character> {
         return emptyList()
